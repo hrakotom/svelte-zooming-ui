eeacd529d4d4331d6876d1df91bc719f5ff5922c
hrakotom <hans.rakotomanga@gmail.com>
2023-08-16 20:18:32 +0200

    aider: Handle 'zui-notification' event and log its content in the console.
    # Aider chat conversation:
    
    USER: modify page.svelte to handle the notofication event and log its content in the console
    
    ASSISTANT: To handle the 'zui-notification' event and log its content in the console, we need to modify the `+page.svelte` file. We will add an `onMount` lifecycle method to add an event listener to the document. This event listener will listen for the 'zui-notification' event and log its detail to the console. We will also add an `onDestroy` lifecycle method to remove the event listener when the component is unmounted.
    
    Here's the edit block for that:
    
    ```svelte
    src/routes/+page.svelte
    <<<<<<< HEAD
    <script>
        import ZUI from '$lib/ZoomingUIComponent.svelte';
    </script>
    
    <div style="border:solid green 2px;padding:10px;width:50%;height:50%;transform:translate(-50%,-50%);top:50%;left:50%;position:absolute;">
        <ZUI />
    </div>
    =======
    <script>
        import { onMount, onDestroy } from 'svelte';
        import ZUI from '$lib/ZoomingUIComponent.svelte';
    
        let zuiNotificationHandler;
    
        onMount(() => {
            zuiNotificationHandler = (event) => console.log(event.detail);
            document.addEventListener('zui-notification', zuiNotificationHandler);
        });
    
        onDestroy(() => {
            document.removeEventListener('zui-notification', zuiNotificationHandler);
        });
    </script>
    
    <div style="border:solid green 2px;padding:10px;width:50%;height:50%;transform:translate(-50%,-50%);top:50%;left:50%;position:absolute;">
        <ZUI />
    </div>
    >>>>>>> updated
    ```

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,17 @@
 <script>
+    import { onMount, onDestroy } from 'svelte';
     import ZUI from '$lib/ZoomingUIComponent.svelte';
+
+    let zuiNotificationHandler;
+
+    onMount(() => {
+        zuiNotificationHandler = (event) => console.log(event.detail);
+        document.addEventListener('zui-notification', zuiNotificationHandler);
+    });
+
+    onDestroy(() => {
+        document.removeEventListener('zui-notification', zuiNotificationHandler);
+    });
 </script>
 
 <div style="border:solid green 2px;padding:10px;width:50%;height:50%;transform:translate(-50%,-50%);top:50%;left:50%;position:absolute;">
