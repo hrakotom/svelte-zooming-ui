04c86a494f39ac2faef249b9a8107b2cb2389a9b
hrakotom <hans.rakotomanga@gmail.com>
2023-08-16 20:17:02 +0200

    removed index.js

--- src/lib/index.js
+++
@@ -1,6 +0,0 @@
-// Reexport your entry components here
-export { default as ZUI } from './ZoomingUIComponent.svelte';
-
-document.addEventListener('zui-notification', (event) => {
-    console.log(event.detail);
-});
