684616095f38f3b8a6254daf95b31b6e31153b22
hrakotom <hans.rakotomanga@gmail.com>
2023-08-22 16:36:35 +0200

    aider: Added a test to check if the ZoomingUIComponent has a random id.
    # Aider chat conversation:
    
    USER: create a first test with a single Zomminguicomponent and check if the id is random
    
    ASSISTANT: To create a test for the ZoomingUIComponent, we need to modify the `tests/test.js` file. However, this file is currently read-only. Could you please add it to the chat so we can make the necessary changes?
    
    USER: I added these *read-write* files: tests/test.js
    
    ASSISTANT: Great! Now we can proceed with creating the test for the ZoomingUIComponent.
    
    We will add a new test case in the `tests/test.js` file. This test will create a ZoomingUIComponent and check if the id is random.
    
    Here is the change:
    
    ```javascript
    tests/test.js
    <<<<<<< HEAD
    import { expect, test } from '@playwright/test';
    
    test('index page has expected h1', async ({ page }) => {
    	await page.goto('/');
    	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
    });
    =======
    import { expect, test } from '@playwright/test';
    import { ZoomingUIComponent } from '../src/ZoomingUIComponent';
    
    test('index page has expected h1', async ({ page }) => {
    	await page.goto('/');
    	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
    });
    
    test('ZoomingUIComponent has random id', async () => {
    	const component1 = new ZoomingUIComponent();
    	const component2 = new ZoomingUIComponent();
    	expect(component1.id).not.toBe(component2.id);
    });
    >>>>>>> updated
    ```
    
    Please note that this change assumes that the ZoomingUIComponent class is located in the `src/ZoomingUIComponent.js` file and it has a public `id` property. If this is not the case, please provide the correct path to the ZoomingUIComponent class and the correct way to access its id.

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,13 @@
 import { expect, test } from '@playwright/test';
+import { ZoomingUIComponent } from '../src/ZoomingUIComponent';
 
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+});
+
+test('ZoomingUIComponent has random id', async () => {
+	const component1 = new ZoomingUIComponent();
+	const component2 = new ZoomingUIComponent();
+	expect(component1.id).not.toBe(component2.id);
 });
