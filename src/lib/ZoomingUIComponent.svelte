56abbda9292d55a10cea9c50109616774346d6a1
hrakotom <hans.rakotomanga@gmail.com>
2023-09-22 17:17:54 +0200

    (no commit message provided)

--- src/lib/ZoomingUIComponent.svelte
+++ src/lib/ZoomingUIComponent.svelte
@@ -1,41 +1,99 @@
 <script>
-    import { onMount, onDestroy, createEventDispatcher } from 'svelte';
-    import { browser } from '$app/environment';
-    import { ui_store, notify } from '$lib//utils.js';
-    import interact from 'interactjs';
-    
+	import { onMount, onDestroy, createEventDispatcher } from 'svelte';
+	import { BROWSER } from 'esm-env';
+	import { ui_store, notify, uuid4 } from '$lib//utils.js';
+	import Decimal from 'decimal.js';
+	import lodash from 'lodash';
+	import interact from 'interactjs';
 
-    export let verbose = false;
+	export let verbose = false;
 
-    // Setup bound dimensions
-    let width = null, height = null;
+	// Setup bound dimensions
+	let width = null,
+		height = null;
 
-    let id = "zui-" + Math.random().toString(36).substring(2) + Math.random().toString(36).substring(2);
+	let id = 'zui-' + uuid4();
+    let resizes = 0;
 
-    // TODO : evaluate context api to pass down variables like : clientWidth and clientHeight ( and uuid of root zui ? )
+	// TODO : evaluate context api to pass down variables like : clientWidth and clientHeight ( and uuid of root zui ? )
 
-    onMount(() => {
-        notify("ZUI mounted");
-        console.log($ui_store.notifications);
-    });
+	onMount(() => {
+		console.log('Checking for ui store');
+		if (!$ui_store.world || !$ui_store.world[id]) {
+			$ui_store.world = $ui_store.world || {};
+			$ui_store.world[id] = {};
+		}
+		// $ui_store._show_debug = true;
+		$ui_store.world[id]._camera_animation = null;
+		// initialize world data
+		$ui_store.world[id].camera = {
+			x: Decimal(0),
+			y: Decimal(0),
+			z: Decimal(0),
+			scale: Decimal(1),
+			w: Decimal(10000),
+			h: Decimal(10000)
+		};
 
-    $: if(browser) {
-        if($ui_store && $ui_store.notifications && $ui_store.notifications.length > 0) {
-            if(!verbose) {
-                $ui_store.notifications = [];
-            } else {
-                while($ui_store.notifications.length > 0) {
-                    console.log($ui_store.notifications.shift());
-                }
-            }
+		$ui_store.world[id]._tgt_camera = {
+			x: Decimal(0),
+			y: Decimal(0),
+			z: Decimal(0),
+			scale: Decimal(1),
+			w: Decimal(10000),
+			h: Decimal(10000)
+		};
+
+		notify('ZUI mounted : ' + JSON.stringify(Object.keys($ui_store)));
+	});
+
+	let resize = lodash.debounce(function (id, width, height) {
+		// console.log("Actual resize call");
+
+		try {
+			$ui_store.world[id]._tgt_camera.w = $ui_store.world[id].camera.w = Decimal(width);
+			$ui_store.world[id]._tgt_camera.h = $ui_store.world[id].camera.h = Decimal(height);
+			$ui_store.world[id]._tgt_camera.fov = $ui_store.world[id].camera.fov = Decimal(
+				(0.5 / Math.tan(Math.PI / 8)) * height
+			);
+			notify('window resized');
+		} catch (e) {
+			console.log('Error on window resize: ' + e);
+		}
+	}, 50);
+
+	$: if(BROWSER){
+	    console.log("Calling resize : " + resizes++);
+        try {
+            resize(id, width, height);
+        } catch (e) {
+            console.log("Error on window resize: " + e);
         }
-    }
+	}
+	$: if (BROWSER && $ui_store.world) {
+		console.log('Current stores: ' + JSON.stringify(Object.keys($ui_store.world)));
+	}
 
+	onDestroy(() => {
+        // console.log("In destroy hook");
+		if ($ui_store && $ui_store.world && $ui_store.world[id]) {
+			// console.log('Killing uid');
+			delete $ui_store.world[id];
+		}
+	});
 
 </script>
 
-
-<div id={id} style="position:absolute;box-sizing:border-box;border:solid red 1px;width:100%;height:100%;top:0px;left:0px;" bind:clientWidth={width} bind:clientHeight={height}>
-    <div style="position:absolute;padding:5px;font-size:xx-small;bottom:-20px;right:1px;font-family:Courier;">{width} x {height}</div>
-    <slot></slot>
+<div
+	{id}
+	style="position:absolute;box-sizing:border-box;border:solid red 1px;width:100%;height:100%;top:0px;left:0px;"
+	bind:clientWidth={width}
+	bind:clientHeight={height}
+>
+	<div
+		style="position:absolute;padding:5px;font-size:xx-small;bottom:-20px;right:1px;font-family:Courier;"
+	>
+		{width} x {height}
+	</div>
+	<slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,27 @@
 <script>
-    // @ts-nocheck
-
+    import { BROWSER } from 'esm-env';
     import ZUI from '$lib/ZoomingUIComponent.svelte';
+    import { ui_store } from '$lib//utils.js';
+
+    let verbose = true;
 
     function handleZuiNotification(event) {
-        console.log(event.detail);
+        console.log("ZUI notification: " + event.detail);
+    }
+    $: if(BROWSER) {
+        if($ui_store && $ui_store.notifications && $ui_store.notifications.length > 0) {
+            if(!verbose) {
+                $ui_store.notifications = [];
+            } else {
+                while($ui_store.notifications.length > 0) {
+                    console.log("INFO: " + $ui_store.notifications.shift().message);
+                }
+            }
+        }
     }
 
 </script>
 
 <div style="border:solid green 2px;width:50%;height:50%;transform:translate(-50%,-50%);top:50%;left:50%;position:absolute;">
-    <ZUI on:zui-notification={handleZuiNotification} verbose=true/>
+    <ZUI on:zui-notification={handleZuiNotification}/>
 </div>
