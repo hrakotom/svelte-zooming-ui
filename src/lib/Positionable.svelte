ab2b8a3d3a06402fde10449ddd516561f02077f8
hrakotom <hans.rakotomanga@gmail.com>
2023-10-13 15:53:24 +0200

    aider: Implemented positionable using utils.js to calculate the position and dimensions of the component based on the camera.
    # Aider chat conversation:
    
    USER: work uot how to best implement positionable using utils.js
    
    ASSISTANT: To implement `Positionable.svelte` using `utils.js`, we can use the `positionable` function from `utils.js` to calculate the position and dimensions of the `Positionable.svelte` component based on the camera defined in `ZoomingUIComponent.svelte`.
    
    Here are the steps to do this:
    
    1. Import the `positionable` function from `utils.js` in `Positionable.svelte`.
    2. Use the `positionable` function in the `onMount` lifecycle hook of `Positionable.svelte` to calculate the position and dimensions of the component.
    3. Update the `style` attribute of the `div` in `Positionable.svelte` based on the calculated position and dimensions.
    
    Here are the changes to `Positionable.svelte`:
    
    ```svelte
    src/lib/Positionable.svelte
    <<<<<<< HEAD
    <script>
        export let x = 0;
        export let y = 0;
        export let width = 0;
        export let height = 0;
    </script>
    
    <div style="position: absolute; left: {x}px; top: {y}px; width: {width}px; height: {height}px;">
        <slot />
    </div>
    =======
    <script>
        import { onMount } from 'svelte';
        import { positionable } from './utils.js';
        import { ui_store } from './utils.js';
    
        export let x = 0;
        export let y = 0;
        export let width = 0;
        export let height = 0;
    
        let node;
    
        onMount(() => {
            const params = {
                x: x,
                y: y,
                width: width,
                height: height,
                camera: $ui_store.world[id].camera
            };
            const positionableInstance = positionable(node, params);
            return () => {
                positionableInstance.destroy();
            };
        });
    </script>
    
    <div bind:this={node} style="position: absolute;">
        <slot />
    </div>
    >>>>>>> updated
    ```
    
    Please note that this is a basic implementation and might need to be adjusted based on your specific needs. For example, you might need to add reactive statements to update the `x`, `y`, `width`, and `height` values based on the state of the camera in `ZoomingUIComponent.svelte`.
    
    Please let me know if you need further adjustments or additional functionality.

--- src/lib/Positionable.svelte
+++ src/lib/Positionable.svelte
@@ -1,10 +1,30 @@
 <script>
+    import { onMount } from 'svelte';
+    import { positionable } from './utils.js';
+    import { ui_store } from './utils.js';
+
     export let x = 0;
     export let y = 0;
     export let width = 0;
     export let height = 0;
+
+    let node;
+
+    onMount(() => {
+        const params = {
+            x: x,
+            y: y,
+            width: width,
+            height: height,
+            camera: $ui_store.world[id].camera
+        };
+        const positionableInstance = positionable(node, params);
+        return () => {
+            positionableInstance.destroy();
+        };
+    });
 </script>
 
-<div style="position: absolute; left: {x}px; top: {y}px; width: {width}px; height: {height}px;">
+<div bind:this={node} style="position: absolute;">
     <slot />
 </div>
