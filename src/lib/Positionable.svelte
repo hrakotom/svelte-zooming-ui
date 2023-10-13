d462993c4a7698a834b52a2318f89543e66e6590
hrakotom <hans.rakotomanga@gmail.com>
2023-10-13 15:49:44 +0200

    aider: Created Positionable.svelte component for adjusting dimensions and position based on the camera defined in ZoomingUIComponent.svelte.
    # Aider chat conversation:
    
    USER: create a component positionable.svelte
    
    ASSISTANT: Sure, I can help you create a new Svelte component named `Positionable.svelte`. However, I need to know what functionality you want in this component. Could you please provide more details?
    
    USER: I would like it to be composable, so that if i put it in a `src/lib/ZoomingUIComponent.svelte`
    
    ASSISTANT: Alright, I understand that you want to create a composable Svelte component named `Positionable.svelte` that can be used within another component `ZoomingUIComponent.svelte`. However, I still need more specific details about what `Positionable.svelte` should do.
    
    For example, should it accept any props? Should it have any specific behavior or render any specific content?
    
    Once I have these details, I can provide the code for the new component.
    
    USER: I added these *read-write* files: src/lib/ZoomingUIComponent.svelte
    
    ASSISTANT: I see that you've added `src/lib/ZoomingUIComponent.svelte` to the list of *read-write* files. However, I still need more specific details about the `Positionable.svelte` component you want to create.
    
    For example, should it accept any props? Should it have any specific behavior or render any specific content?
    
    Once I have these details, I can provide the code for the new component.
    
    USER: it should be reacting match its dimensions and position according to the camera defined in zomminui
    
    ASSISTANT: Alright, I understand that you want the `Positionable.svelte` component to adjust its dimensions and position according to the camera defined in `ZoomingUIComponent.svelte`.
    
    Here's a basic structure for the `Positionable.svelte` component. This component will accept `x`, `y`, `width`, and `height` as props and will adjust its style accordingly.
    
    ```svelte
    src/lib/Positionable.svelte
    <<<<<<< HEAD
    
    =======
    <script>
        export let x = 0;
        export let y = 0;
        export let width = 0;
        export let height = 0;
    </script>
    
    <div style="position: absolute; left: {x}px; top: {y}px; width: {width}px; height: {height}px;">
        <slot />
    </div>
    >>>>>>> updated
    ```
    
    This is a very basic implementation and might need to be adjusted based on your specific needs. For example, you might need to add reactive statements to update the `x`, `y`, `width`, and `height` values based on the state of the camera in `ZoomingUIComponent.svelte`.
    
    Please let me know if you need further adjustments or additional functionality.

---
+++ src/lib/Positionable.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let x = 0;
+    export let y = 0;
+    export let width = 0;
+    export let height = 0;
+</script>
+
+<div style="position: absolute; left: {x}px; top: {y}px; width: {width}px; height: {height}px;">
+    <slot />
+</div>
