0b56126636f2b1358f23856072418693d43d159d
hrakotom <hans.rakotomanga@gmail.com>
2023-11-19 16:07:04 +0100

    Refactored the import and usage of the `resizeObserved` function in the `ZoomingUIComponent` component.

--- src/lib/ZoomingUIComponent.svelte
+++ src/lib/ZoomingUIComponent.svelte
@@ -2,18 +2,12 @@
 	import { onMount, onDestroy, createEventDispatcher, setContext } from 'svelte';
 	import { writable } from 'svelte/store';
 	import { BROWSER } from 'esm-env';
-	import { notify, uuid4, positioned } from '$lib//utils.js';
+	import { notify, uuid4, resizeObserved } from '$lib//utils.js';
 	import Decimal from 'decimal.js';
 	import lodash from 'lodash';
 	import interact from 'interactjs';
     import anime from "animejs";
 	
-	// Setup bound dimensions and positions
-	let width = null,
-		height = null,
-		x = null,
-		y = null;
-
 	let id = 'zui-' + uuid4();
 	let resizes = 0;
 
@@ -24,8 +18,6 @@
 		h: Decimal(0)
 	});
 	setContext('screen', screen);
-
-	// TODO : evaluate context api to pass down variables like : clientWidth and clientHeight ( and uuid of root zui ? )
 
 	onMount(() => {
 	});
@@ -38,7 +30,7 @@
 <div
 	{id}
 	style="position:absolute;box-sizing:border-box;border:solid red 1px;width:100%;height:100%;top:0px;left:0px;"
-		use:positioned={screen}
+		use:resizeObserved={screen}
 	>
 		<pre style="font-family:'Courier New', Courier, monospace;font-size:10px;">
 			{ JSON.stringify($screen, null, ' ') }
