224fa09ad2b77484543c222633cd7d2f25d7b702
hrakotom <hans.rakotomanga@gmail.com>
2023-11-22 15:04:56 +0100

    adding components

---
+++ src/lib/Clickable.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { getContext } from "svelte";
+    import { fade } from 'svelte/transition';
+
+    export let bgcolor = "red";
+
+    let focus = getContext("focusOn");
+    let frame = getContext("frame");
+
+    function clicked(evt) {
+        console.log("Clicked: " + JSON.stringify($frame));
+        focus($frame.x, $frame.y, $frame.width, $frame.height);
+    }
+
+</script>
+
+
+<div style="width:100%;height:100%;background-color:{bgcolor};cursor:pointer;" on:click={clicked} in:fade>
+    <slot></slot>
+</div>
---
+++ src/lib/Embedded.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { getContext } from "svelte";
+    import { fade } from 'svelte/transition';
+    import Positionable from '$lib/Positionable.svelte';
+    import Decimal from 'decimal.js';
+
+    export let bgcolor = "red";
+
+    let focus = getContext("focusOn");
+    let frame = getContext("frame");
+
+    function clicked(evt) {
+        console.log("Clicked: " + JSON.stringify($frame));
+        focus($frame.x.plus($frame.width.div(4)), $frame.y.plus($frame.height.div(4)), $frame.width.div(4), $frame.height.div(4));
+    }
+
+</script>
+
+<Positionable x={$frame.x.plus($frame.width.div(4))} y={$frame.y.plus($frame.height.div(4))} width={$frame.width.div(4)} height={$frame.height.div(4)} depth={Decimal(2)}>
+    <div style="background-color:rgba(0,0,0,0.5);color:white;width:100%;height:100%;font-weight:400;font-family:Courier Prime;display:flex;justify-content:center;align-items:center;font-size:61px;cursor:pointer;border-radius:4%;" on:click={clicked}>Embedded</div>
+</Positionable>
+<Positionable x={$frame.x.plus($frame.width.div(4))} y={$frame.y.minus(2)} width={$frame.width.div(4)} height={$frame.height.div(4)} depth={Decimal(2)}>
+    <div style="background-color:rgba(0,0,0,0.5);color:white;width:100%;height:100%;font-weight:400;font-family:Courier Prime;display:flex;justify-content:center;align-items:center;font-size:61px;cursor:pointer;border-radius:4%;" on:click={clicked}>Clicking me sends above.</div>
+</Positionable>
+
