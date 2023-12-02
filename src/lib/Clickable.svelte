796177220b6562798a422bff71425521116400cf
hrakotom <hans.rakotomanga@gmail.com>
2023-12-02 20:44:54 +0100

    Changed Clickable.svelte and Embedded.svelte to add fade transition and border to the clickable div, and update the styling of the embedded div.

--- src/lib/Clickable.svelte
+++ src/lib/Clickable.svelte
@@ -8,13 +8,13 @@
     let frame = getContext("frame");
 
     function clicked(evt) {
-        console.log("Clicked: " + JSON.stringify($frame));
+        //console.log("Clicked: " + JSON.stringify($frame));
         focus($frame.x, $frame.y, $frame.width, $frame.height);
     }
 
 </script>
 
 
-<div style="width:100%;height:100%;background-color:{bgcolor};cursor:pointer;" on:click={clicked} in:fade>
+<div style="width:100%;height:100%;background-color:{bgcolor};cursor:pointer;border:solid rgba(0,0,0,1) 0px;box-sizing:border-box;" on:click={clicked} in:fade>
     <slot></slot>
 </div>
--- src/lib/Embedded.svelte
+++ src/lib/Embedded.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { getContext } from "svelte";
+    import { getContext, onMount } from "svelte";
     import { fade } from 'svelte/transition';
     import Positionable from '$lib/Positionable.svelte';
     import Decimal from 'decimal.js';
@@ -13,13 +13,12 @@
         console.log("Clicked: " + JSON.stringify($frame));
         focus($frame.x.plus($frame.width.div(4)), $frame.y.plus($frame.height.div(4)), $frame.width.div(4), $frame.height.div(4));
     }
-
 </script>
-
+{#if $frame.factor && $frame.factor.greaterThan(0.4)}
 <Positionable x={$frame.x.plus($frame.width.div(4))} y={$frame.y.plus($frame.height.div(4))} width={$frame.width.div(4)} height={$frame.height.div(4)} depth={Decimal(2)}>
     <div style="background-color:rgba(0,0,0,0.5);color:white;width:100%;height:100%;font-weight:400;font-family:Courier Prime;display:flex;justify-content:center;align-items:center;font-size:61px;cursor:pointer;border-radius:4%;" on:click={clicked}>Embedded</div>
 </Positionable>
 <Positionable x={$frame.x.plus($frame.width.div(4))} y={$frame.y.minus(2)} width={$frame.width.div(4)} height={$frame.height.div(4)} depth={Decimal(2)}>
-    <div style="background-color:rgba(0,0,0,0.5);color:white;width:100%;height:100%;font-weight:400;font-family:Courier Prime;display:flex;justify-content:center;align-items:center;font-size:61px;cursor:pointer;border-radius:4%;" on:click={clicked}>Clicking me sends above.</div>
+    <div in:fade style="background-color:rgba(0,0,0,0.5);color:white;width:100%;height:100%;font-weight:400;font-family:Courier Prime;display:flex;justify-content:center;align-items:center;font-size:61px;cursor:pointer;border-radius:4%;text-align:center;" on:click={clicked}>Clicking me sends above.</div>
 </Positionable>
-
+{/if}
